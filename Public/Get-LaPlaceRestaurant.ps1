3e17a9346b3c2644ab7a95f8357cd71fd9cc4771
Jan Hoek <github@uncommonsense.nl>
2025-08-21 21:47:53 +0200

    Preparations

--- Public/Get-LaPlaceRestaurant.ps1
+++ Public/Get-LaPlaceRestaurant.ps1
@@ -1,6 +1,37 @@
 function Get-LaPlaceRestaurant
 {
+    [Parameter(Mandatory, Position = 0)]
     param
     (
+        [string]$ID
     )
+
+    $Content = Invoke-WebRequest -Uri "https://www.laplace.com/locaties/$ID"
+    | Select-Object -ExpandProperty Content
+
+    if ($Content -match '<script id="__NEXT_DATA__" type="application/json">(.*)</script>')
+    {
+        $Data = $Matches[1]
+        | ConvertFrom-Json
+        | Select-Object -ExpandProperty props
+        | Select-Object -ExpandProperty pageProps
+        | Select-Object -ExpandProperty data
+
+        [PSCustomObject]@{
+            Name      = $Data.Title
+            Latitude  = $Data.position.Latitude
+            Longitude = $Data.position.Longitude
+            Address   = @($Data.Address.Street, (@($Data.Address.HouseNr, $Data.Address.HouseSub) -join '') | Where-Object { $_ }) -join ' '
+            Postcode  = $Data.Address.PostalCode
+            City      = $Data.Address.City
+            Country   = $Data.Address.Country
+            PhoneNo   = $Data.Contact.Phone
+            OpeningHours = [PSCustomObject]@{
+                Monday = $Data.OpeningHours.Monday | ForEach-Object { [PSCustomObject]@{
+                    From = [TimeSpan]$_.FromTime
+                    To = [TimeSpan]$_.ToTime
+                }}
+            }
+        }
+    }
 }
--- UncommonSense.LaPlace.Tests.ps1
+++ UncommonSense.LaPlace.Tests.ps1
@@ -1,6 +1,15 @@
 Describe 'UncommonSense.LaPlace' {
 	Context 'Get-LaPlaceRestaurant' {
-		It 'works' {
+		It 'Retrieves LaPlace Muiden restaurant information' {
+            $Restaurant = Get-LaPlaceRestaurant -ID 'la-place-muiden-de-hackelaar-a1'
+            $Restaurant.Name | Should -Be 'La Place Muiden De Hackelaar A1/A6'
+            $Restaurant.Latitude | Should -Be 52.322832
+            $Restaurant.Longitude | Should -Be 5.081649
+            $Restaurant.Address | Should -Be 'Rijksweg A1 NZ 3'
+            $Restaurant.PostCode | Should -Be '1398 PW'
+            $Restaurant.City | Should -Be 'Muiden'
+            $Restaurant.Country | Should -Be 'NL'
+            $Restaurant.PhoneNo | Should -Be '+31630314327'
 		}
 	}
 }
